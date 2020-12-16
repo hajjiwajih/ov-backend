367e07016291b0676031f2bb1a733d1586e0d090
Anis-Abaza <62609780+Anis-Abaza@users.noreply.github.com>
2020-12-16 23:22:01 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -34,3 +34,16 @@ boot(app, __dirname, function (err) {
     app.start();
 });
 
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+// Probe every 5th second.
+collectDefaultMetrics({ timeout: 5000 });
+
+const counter = new client.Counter({
+  name: 'node_request_operations_total',
+  help: 'The total number of processed requests'
+});
+// Metrics endpoint
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', client.register.contentType)
+  res.end(client.register.metrics())
