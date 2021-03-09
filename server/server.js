38a373fbd2aaa056ad7b84adc0cd1bd75f4e0c50
Anis-Abaza <62609780+Anis-Abaza@users.noreply.github.com>
2021-03-09 23:12:23 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ app.start = function () {
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function (err) {
   if (err) throw err;
-  // start the server if `$ node server.js`
+   //start the server if `$ node server.js`
   if (require.main === module)
     app.start();
     
